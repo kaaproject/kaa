83177ebcf936f638364d0cf9a11058c426904d9a
Andrew Shvayka <ashvayka@cybervisiontech.com>
2014-10-31 18:37:22 +0200

    KAA-114: CDAP Appender Implementation

--- server/appenders/cdap/appender/src/main/java/org/kaaproject/kaa/server/appenders/cdap/appender/CdapLogAppender.java
+++ server/appenders/cdap/appender/src/main/java/org/kaaproject/kaa/server/appenders/cdap/appender/CdapLogAppender.java
@@ -16,42 +16,115 @@
 
 package org.kaaproject.kaa.server.appenders.cdap.appender;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
+import org.kaaproject.kaa.common.dto.logs.LogEventDto;
 import org.kaaproject.kaa.server.common.log.shared.appender.CustomLogAppender;
 import org.kaaproject.kaa.server.common.log.shared.appender.LogEventPack;
 import org.kaaproject.kaa.server.common.log.shared.avro.gen.RecordHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import co.cask.cdap.client.StreamClient;
+import co.cask.cdap.client.StreamWriter;
+import co.cask.cdap.client.rest.RestStreamClient;
+import co.cask.cdap.security.authentication.client.AuthenticationClient;
+
 public class CdapLogAppender extends CustomLogAppender {
 
+    /**
+     * Configuration properties constants
+     */
+    private static final String STREAM = "stream";
+    private static final String HOST = "host";
+    private static final String PORT = "port";
+    private static final String SSL = "ssl";
+    private static final String VERIFY_SSL_CERT = "verify_SSL_CERT";
+    private static final String VERSION = "version";
+    private static final String AUTH_CLIENT = "auth_client";
+
+    /**
+     * Default values
+     */
+    private static final String DEFAULT_HOST = "localhost";
+    private static final String DEFAULT_PORT = "10000";
+    private static final String DEFAULT_SSL = "false";
+
     private static final Logger LOG = LoggerFactory.getLogger(CdapLogAppender.class);
+    private static final Charset UTF8 = Charset.forName("UTF-8");
 
     private boolean closed = false;
 
-    public CdapLogAppender() {
-    }
+    private StreamClient streamClient;
+    private StreamWriter streamWriter;
 
-    @Override
-    public void close() {
-        closed = true;
-        LOG.debug("Stopped Cdap log appender.");
+    public CdapLogAppender() {
+        super();
     }
 
     @Override
     public void doAppend(LogEventPack logEventPack, RecordHeader header) {
-        if (!closed) {
-            LOG.debug("[{}] appending {} logs using cdap appender", logEventPack.getEvents().size());
-            //List<LogEventDto> dtos = generateLogEvent(logEventPack, header);
+        if (!closed && streamWriter != null) {
+            LOG.debug("[{}] appending {} logs to cdap stream", this.getApplicationToken(), logEventPack.getEvents().size());
+            for(LogEventDto dto : generateLogEvent(logEventPack, header)){
+                StringBuilder sb = new StringBuilder();
+                sb.append("{\"header\":").append(dto.getHeader()).append(",");
+                sb.append("\"event\":").append(dto.getEvent()).append("}");
+                streamWriter.write(sb.toString(), UTF8);
+            }
         } else {
             LOG.info("Attempted to append to closed appender named [{}].", getName());
         }
     }
 
     @Override
-    protected void initFromProperties(Properties properties) {
-        // TODO Auto-generated method stub
+    protected void initFromProperties(Properties properties){
+        try {
+            streamClient = initStreamClient(properties);
+            if(properties.containsKey(STREAM)){
+                streamWriter = streamClient.createWriter(properties.getProperty(STREAM));
+            }else{
+                throw new IllegalArgumentException(STREAM + " property is not set!");
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to init stream client: ", e);
+        }
     }
- 
+    
+    @Override
+    public void close() {
+        closed = true;
+        try {
+            streamClient.close();
+        } catch (IOException e) {
+            LOG.error("Failed to close stream client: ", e);
+        }
+        LOG.debug("Stopped Cdap log appender.");
+    }    
+    
+    private StreamClient initStreamClient(Properties properties) throws Exception{
+        String host = properties.getProperty(HOST, DEFAULT_HOST);
+        Integer port = Integer.valueOf(properties.getProperty(PORT, DEFAULT_PORT));
+        boolean ssl = Boolean.parseBoolean(properties.getProperty(SSL, DEFAULT_SSL));
+        RestStreamClient.Builder builder = RestStreamClient.builder(host, port).ssl(ssl);
+        
+        if(properties.containsKey(VERSION)){
+            builder.version(properties.getProperty(VERSION));
+        }
+        if(properties.containsKey(VERIFY_SSL_CERT)){
+            builder.verifySSLCert(Boolean.parseBoolean(properties.getProperty(VERIFY_SSL_CERT)));
+        }
+        if(properties.containsKey(AUTH_CLIENT)){
+            AuthenticationClient authClient =
+                    (AuthenticationClient) Class.forName(properties.getProperty(AUTH_CLIENT)).getConstructor().newInstance();
+            authClient.setConnectionInfo(host, port, ssl);
+            if (authClient.isAuthEnabled()) {
+                authClient.configure(properties);
+            }
+            builder.authClient(authClient);
+        }
+        return builder.build();
+    }
 }
