7a47453fa0ad6edca8e51b86b3a1df1f7d6768f5
Yaroslav Zeygerman <yzeygerman@cybervisiontech.com>
2014-11-27 15:49:03 +0200

    KAA-233: Fix the crash of C++ client on configuration update

--- client/client-multi/client-cpp/test/impl/configuration/ConfigurationPersistenceTest.cpp
+++ client/client-multi/client-cpp/test/impl/configuration/ConfigurationPersistenceTest.cpp
@@ -29,7 +29,7 @@ class ConfigurationProcessorStub : public IConfigurationProcessor
 {
 public:
     ConfigurationProcessorStub() : processConfigurationCalled_(false), schemaUpdatedCalled_(false) {}
-    void processConfigurationData(const std::uint8_t *data, std::size_t data_length, bool full_resync)
+    void processConfigurationData(const std::uint8_t *data, std::size_t dataLength, bool fullResync)
     {
         processConfigurationCalled_ = true;
     }
@@ -160,6 +160,51 @@ BOOST_AUTO_TEST_CASE(checkConfigurationLoad)
     } catch (...) {
         BOOST_CHECK(false);
     }
+}
+
+BOOST_AUTO_TEST_CASE(checkConfigurationLoadWithoutSchema)
+{
+    std::shared_ptr<avro::ValidSchema> schema(new avro::ValidSchema(
+             avro::compileJsonSchemaFromMemory(reinterpret_cast<const std::uint8_t *>(sch.c_str()), sch.length())));
+    uuid_t uuid = {{0,1,2,3,4}};
+    std::vector<std::uint8_t> uuid_vec = {0,1,2,3,4};
+    CommonRecord rec(uuid, schema->root());
+    std::string testField1("string");
+    avro::GenericDatum sd(testField1);
+    rec.setField("testField1", CommonTypesFactory::createCommon<avro::AVRO_STRING>(sd));
+    size_t uuid_index;
+    schema->root()->nameIndex("__uuid", uuid_index);
+    avro::GenericDatum ud(schema->root()->leafAt(uuid_index));
+    avro::GenericFixed uuid_fixed_field(schema->root()->leafAt(uuid_index));
+    uuid_fixed_field.value() = uuid_vec;
+    ud.value<avro::GenericFixed>() = uuid_fixed_field;
+    rec.setField("__uuid", CommonTypesFactory::createCommon<avro::AVRO_FIXED>(ud));
+
+    ConfigurationPersistenceManager cpm;
+    BOOST_REQUIRE_THROW(cpm.onConfigurationUpdated(rec), KaaException);
+
+    std::shared_ptr<avro::ValidSchema> root_schema(new avro::ValidSchema(
+            avro::compileJsonSchemaFromMemory(reinterpret_cast<const std::uint8_t *>(root_sch.c_str()), root_sch.length())));
+    ConfigurationProcessorStub cpstub;
+
+    CoonfigurationStorageStub csstub;
+    cpm.setConfigurationProcessor(&cpstub);
+    cpm.setConfigurationStorage(&csstub);
+    cpm.onSchemaUpdated(root_schema);
+
+    BOOST_CHECK(csstub.isLoadCalled());
+    BOOST_CHECK(!csstub.isSaveCalled());
+    BOOST_CHECK(cpstub.isProcessConfigurationCalled());
+
+    cpm.onConfigurationUpdated(rec);
+    BOOST_CHECK(!csstub.isSaveCalled());
+
+    cpm.onConfigurationUpdated(rec);
+    BOOST_CHECK(csstub.isSaveCalled());
+
+    BOOST_CHECK(cpm.getConfigurationHash().getHash().first.get() != nullptr);
+    BOOST_CHECK(cpm.getConfigurationHash().getHash().second > 0);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
