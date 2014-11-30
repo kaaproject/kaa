1385a3256dbaf10240e90f8baa8626abf7d3dc4b
Andrew Kokhanovskyi <akokhanovskyi@cybervisiontech.com>
2014-11-30 17:06:54 +0200

    KAA-229: cleanup kaa_common.*, add TODOs for further refactoring

--- client/client-multi/client-c/src/kaa_common.h
+++ client/client-multi/client-c/src/kaa_common.h
@@ -17,24 +17,15 @@
 #ifndef KAA_COMMON_H_
 #define KAA_COMMON_H_
 
-#ifdef __cplusplus
-extern "C" {
-#define CLOSE_EXTERN }
-#else
-#define CLOSE_EXTERN
-#endif
-
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "kaa_error.h"
 
-typedef enum kaa_server_response_result_t {
-    KAA_SUCCESS,
-    KAA_FAILURE
-} kaa_server_response_result_t;
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define KAA_RETURN_IF_NIL(p, E) \
     { if (!(p)) return (E); }
@@ -48,15 +39,15 @@ typedef enum kaa_server_response_result_t {
 #define KAA_RETURN_IF_NIL4(p1, p2, p3, p4, E) \
     { if (!(p1) || !(p2) || !(p3) || !(p4)) return (E); }
 
-
-typedef struct kaa_attachment_status_listeners_t
-{
-    void (* on_attached_callback)(const char * user_external_id, const char * endpoint_access_token);
-    void (* on_detached_callback)(const char * endpoint_access_token);
-    void (* on_response_callback)(bool is_attached);
+// TODO: move to kaa_user.h
+typedef struct kaa_attachment_status_listeners_t {
+    void (*on_attached_callback)(const char *user_external_id, const char *endpoint_access_token);
+    void (*on_detached_callback)(const char *endpoint_access_token);
+    void (*on_response_callback)(bool is_attached);
 } kaa_attachment_status_listeners_t;
 
-typedef void (* event_callback_t)(const char * event_fqn, const char * event_data, size_t event_data_size);
+// TODO: move to kaa_event.h
+typedef void (*event_callback_t)(const char *event_fqn, const char *event_data, size_t event_data_size);
 
 typedef enum {
     KAA_SERVICE_BOOTSTRAP = 0,
@@ -66,32 +57,29 @@ typedef enum {
     KAA_SERVICE_LOGGING = 4,
 } kaa_service_t;
 
+// TODO: move these two to kaa_channel_manager.h
 typedef void (*kaa_sync_t)(size_t service_count, const kaa_service_t services[]);
 typedef void (*kaa_sync_all_t)();
 
 /**
- * Hash calculating stuff
+ * SHA1 hash
  */
 #define SHA_1_DIGEST_LENGTH 20
 typedef unsigned char kaa_digest[SHA_1_DIGEST_LENGTH];
-/**
- * A size of a hash buffer must be not less than SHA_1_DIGEST_LENGTH
- */
 kaa_error_t kaa_calculate_sha_hash(const char *data, size_t data_size, kaa_digest digest);
 
-typedef enum kaa_channel_type_t {
+// TODO: move to kaa_channel_manager.h and do something about the kaa_defaults.h
+#define KAA_CHANNEL_TYPE_COUNT 3
+typedef enum {
     HTTP,
     HTTP_LP,
     KAATCP
 } kaa_channel_type_t;
 
-/**
- * Define's value should be equal to a number of
- * elements in kaa_channel_type_t enumeration
- */
-#define KAA_CHANNEL_TYPE_COUNT   3
-
+// TODO: move to kaa_event.h
 typedef size_t kaa_trx_id;
 
-CLOSE_EXTERN
+#ifdef __cplusplus
+}      /* extern "C" */
+#endif
 #endif /* KAA_COMMON_H_ */
