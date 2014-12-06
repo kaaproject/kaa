d5188b1294368c5b5f1b938230534a3d965d3582
Andrew Kokhanovskyi <akokhanovskyi@cybervisiontech.com>
2014-12-06 11:31:39 +0200

    KAA-229: deliberately remove brackets around "(E)" in KAA_RETURN_IF_NIL* macros to allow empty macro parameter in void functions. For example: KAA_RETURN_IF_NIL(self,); now translates into: if (!self)     return;

--- client/client-multi/client-c/src/kaa_common.h
+++ client/client-multi/client-c/src/kaa_common.h
@@ -28,16 +28,16 @@ extern "C" {
 #endif
 
 #define KAA_RETURN_IF_NIL(p, E) \
-    { if (!(p)) return (E); }
+    { if (!(p)) return E; }
 
 #define KAA_RETURN_IF_NIL2(p1, p2, E) \
-    { if (!(p1) || !(p2)) return (E); }
+    { if (!(p1) || !(p2)) return E; }
 
 #define KAA_RETURN_IF_NIL3(p1, p2, p3, E) \
-    { if (!(p1) || !(p2) || !(p3)) return (E); }
+    { if (!(p1) || !(p2) || !(p3)) return E; }
 
 #define KAA_RETURN_IF_NIL4(p1, p2, p3, p4, E) \
-    { if (!(p1) || !(p2) || !(p3) || !(p4)) return (E); }
+    { if (!(p1) || !(p2) || !(p3) || !(p4)) return E; }
 
 // TODO: move to kaa_event.h
 
