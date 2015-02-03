09b5556e1fb306733d9b846d65b4bda25daa9f3d
Viktor Batytskyi <vbatytskyi@cybervisiontech.com>
2015-02-03 19:42:15 +0200

    KAA-331: update FacebookVerifierConfig in accordance with PluginConfig interface

--- server/verifiers/facebook-verifier/config/src/main/java/org/kaaproject/kaa/server/verifiers/facebook/config/FacebookVerifierConfig.java
+++ server/verifiers/facebook-verifier/config/src/main/java/org/kaaproject/kaa/server/verifiers/facebook/config/FacebookVerifierConfig.java
@@ -2,33 +2,28 @@ package org.kaaproject.kaa.server.verifiers.facebook.config;
 
 
 import org.apache.avro.Schema;
-import org.kaaproject.kaa.server.common.utils.CRC32Util;
-import org.kaaproject.kaa.server.common.verifier.KaaUserVerifierConfig;
-import org.kaaproject.kaa.server.common.verifier.UserVerifierConfig;
+import org.kaaproject.kaa.server.common.plugin.KaaPluginConfig;
+import org.kaaproject.kaa.server.common.plugin.PluginConfig;
+import org.kaaproject.kaa.server.common.plugin.PluginType;
 import org.kaaproject.kaa.server.verifiers.facebook.config.gen.FacebookAvroConfig;
 
-@KaaUserVerifierConfig
-public class FacebookVerifierConfig implements UserVerifierConfig {
+@KaaPluginConfig(pluginType = PluginType.USER_VERIFIER)
+public class FacebookVerifierConfig implements PluginConfig {
 
-    private static final String ORG_KAAPROJECT_KAA_SERVER_VERIFIERS_TRUSTFUL = "org.kaaproject.kaa.server.verifiers.facebook";
+    private static final String TRUSTFUL_VERIFIER_NAME = "Facebook verifier";
 
     @Override
-    public int getId() {
-        return CRC32Util.crc32(getName());
+    public String getPluginTypeName() {
+        return TRUSTFUL_VERIFIER_NAME;
     }
 
     @Override
-    public String getName() {
-        return ORG_KAAPROJECT_KAA_SERVER_VERIFIERS_TRUSTFUL;
+    public String getPluginClassName() {
+        return "org.kaaproject.kaa.server.verifiers.facebook.FacebookUserVerifier";
     }
 
     @Override
-    public String getUserVerifierClass() {
-        return "org.kaaproject.kaa.server.verifiers.trustful.TrustfulUserVerifier";
-    }
-
-    @Override
-    public Schema getConfigSchema() {
+    public Schema getPluginConfigSchema() {
         return FacebookAvroConfig.SCHEMA$;
     }
 }
