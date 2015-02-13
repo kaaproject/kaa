52dd2340aca4813a32bdaffbd2f77fe02e75a4ff
Andrew Shvayka <ashvayka@cybervisiontech.com>
2015-02-13 12:12:58 +0200

    KAA-344: removed redundant autogenerated files

--- examples/smarthousedemo/source/gen/org/kaaproject/kaa/demo/smarthousedemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.kaaproject.kaa.demo.smarthousedemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
