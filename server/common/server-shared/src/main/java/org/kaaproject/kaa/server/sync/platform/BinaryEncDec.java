4ea7c19f9d5881bbf51768ef80db6189691f1b38
Yaroslav Zeygerman <yzeygerman@cybervisiontech.com>
2015-02-20 17:54:33 +0200

    KAA-296: Update README and CMake file according to the Kaa Configuration subunit

--- server/common/server-shared/src/main/java/org/kaaproject/kaa/server/sync/platform/BinaryEncDec.java
+++ server/common/server-shared/src/main/java/org/kaaproject/kaa/server/sync/platform/BinaryEncDec.java
@@ -394,10 +394,10 @@ public class BinaryEncDec implements PlatformEncDec {
             buf.putInt(configurationSync.getConfDeltaBody().array().length);
         }
         if (confSchemaPresent) {
-            buf.put(configurationSync.getConfSchemaBody().array());
+            put(buf, configurationSync.getConfSchemaBody().array());
         }
         if (confBodyPresent) {
-            buf.put(configurationSync.getConfDeltaBody().array());
+            put(buf, configurationSync.getConfDeltaBody().array());
         }
 
         buf.putInt(extPosition - SIZE_OF_INT, buf.position() - extPosition);
@@ -461,8 +461,8 @@ public class BinaryEncDec implements PlatformEncDec {
                 buf.putShort(response.getResult() == SyncStatus.SUCCESS ? SUCCESS : FAILURE);
                 if (response.getListeners() != null) {
                     buf.putInt(response.getListeners().size());
-                    for (String listner : response.getListeners()) {
-                        buf.put(Base64Util.decode(listner));
+                    for (String listener : response.getListeners()) {
+                        put(buf, Base64Util.decode(listener));
                     }
                 } else {
                     buf.putInt(0);
