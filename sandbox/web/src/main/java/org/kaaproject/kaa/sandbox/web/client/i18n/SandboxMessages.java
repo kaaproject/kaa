de64b22bdc58369cffd9e1234d423288fa7d73f6
Viktor Batytskyi <vbatytskyi@cybervisiontech.com>
2015-03-05 23:12:21 +0200

    KAA-380,381 - fix newlines and autogenerated paths

--- sandbox/web/src/main/java/org/kaaproject/kaa/sandbox/web/client/i18n/SandboxMessages.java
+++ sandbox/web/src/main/java/org/kaaproject/kaa/sandbox/web/client/i18n/SandboxMessages.java
@@ -2,7 +2,7 @@ package org.kaaproject.kaa.sandbox.web.client.i18n;
 
 /**
  * Interface to represent the messages contained in resource bundle:
- * 	/home/vii/Projects/Kaa/sandbox/web/src/main/java/org/kaaproject/kaa/sandbox/web/client/i18n/SandboxMessages.properties'.
+ * 	D:/git/kaa_github/sandbox/web/src/main/java/org/kaaproject/kaa/sandbox/web/client/i18n/SandboxMessages.properties'.
  */
 public interface SandboxMessages extends com.google.gwt.i18n.client.Messages {
   
