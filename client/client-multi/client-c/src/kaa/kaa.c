c6000d7adf6cdb06f59373f10ae9aca3e96eb1e0
Yaroslav Zeygerman <yzeygerman@cybervisiontech.com>
2015-03-26 13:03:35 +0200

    KAA-457: Fix configuration manager ceration process

--- client/client-multi/client-c/src/kaa/kaa.c
+++ client/client-multi/client-c/src/kaa/kaa.c
@@ -65,7 +65,8 @@ extern void kaa_log_collector_destroy(kaa_log_collector_t *self);
 
 #ifndef KAA_DISABLE_FEATURE_CONFIGURATION
 extern kaa_error_t kaa_configuration_manager_create(kaa_configuration_manager_t **configuration_manager_p,
-                                                    kaa_status_t *status, kaa_logger_t *logger);
+                                                    kaa_channel_manager_t *channel_manager, kaa_status_t *status,
+                                                    kaa_logger_t *logger);
 extern void kaa_configuration_manager_destroy(kaa_configuration_manager_t *self);
 #endif
 
@@ -134,7 +135,7 @@ static kaa_error_t kaa_context_create(kaa_context_t **context_p, kaa_logger_t *l
 
 #ifndef KAA_DISABLE_FEATURE_CONFIGURATION
     if (!error)
-        error = kaa_configuration_manager_create(&((*context_p)->configuration_manager),
+        error = kaa_configuration_manager_create(&((*context_p)->configuration_manager), (*context_p)->channel_manager,
                                                  (*context_p)->status->status_instance, (*context_p)->logger);
 #else
     (*context_p)->configuration_manager = NULL;
