54da954d0c2feb50a0e555027d722987c9c89bf9
Viktor Batytskyi <vbatytskyi@cybervisiontech.com>
2015-03-31 19:27:48 +0300

    update test for EntityConvertUtils

--- server/operations/src/test/java/org/kaaproject/kaa/server/operations/service/akka/utils/EntityConvertUtilsTest.java
+++ server/operations/src/test/java/org/kaaproject/kaa/server/operations/service/akka/utils/EntityConvertUtilsTest.java
@@ -20,6 +20,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.kaaproject.kaa.server.common.log.shared.appender.LogDeliveryErrorCode;
 import org.kaaproject.kaa.server.common.verifier.UserVerifierErrorCode;
+import org.kaaproject.kaa.server.operations.service.akka.messages.core.logs.LogDeliveryMessage;
+import org.kaaproject.kaa.server.operations.service.akka.messages.core.user.verification.UserVerificationResponseMessage;
+import org.kaaproject.kaa.server.sync.LogDeliveryStatus;
+import org.kaaproject.kaa.server.sync.LogServerSync;
+import org.kaaproject.kaa.server.sync.SyncStatus;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class EntityConvertUtilsTest {
@@ -53,5 +66,35 @@ public class EntityConvertUtilsTest {
                 org.kaaproject.kaa.server.sync.LogDeliveryErrorCode.REMOTE_CONNECTION_ERROR);
         Assert.assertEquals(EntityConvertUtils.toErrorCode(LogDeliveryErrorCode.REMOTE_INTERNAL_ERROR),
                 org.kaaproject.kaa.server.sync.LogDeliveryErrorCode.REMOTE_INTERNAL_ERROR);
+    }
+
+    @Test
+    public void convertTest() {
+        UserVerificationResponseMessage successMessage = getUserVerificationResponseMessage(true, null);
+        UserVerificationResponseMessage failureMessage = getUserVerificationResponseMessage(false, null);
+        Assert.assertEquals(EntityConvertUtils.convert(successMessage).getResult(), SyncStatus.SUCCESS);
+        Assert.assertEquals(EntityConvertUtils.convert(failureMessage).getResult(), SyncStatus.FAILURE);
+        Map<Integer, LogDeliveryMessage> responseMap = new HashMap<>();
+        LogDeliveryMessage successLogDeliveryMessage = getLogDeliveryMessage(true, null);
+        LogDeliveryMessage failureLogDeliveryMessage = getLogDeliveryMessage(false,
+                LogDeliveryErrorCode.APPENDER_INTERNAL_ERROR);
+        responseMap.put(1, successLogDeliveryMessage);
+        responseMap.put(2, failureLogDeliveryMessage);
+        LogServerSync sync = EntityConvertUtils.convert(responseMap);
+        Assert.assertNull(sync.getDeliveryStatuses().get(0).getErrorCode());
+        Assert.assertEquals(sync.getDeliveryStatuses().get(1).getErrorCode(),
+                org.kaaproject.kaa.server.sync.LogDeliveryErrorCode.APPENDER_INTERNAL_ERROR);
+    }
+
+    private UserVerificationResponseMessage getUserVerificationResponseMessage(boolean status,
+                                                    UserVerifierErrorCode errorCode) {
+        UserVerificationResponseMessage responseMessage = mock(UserVerificationResponseMessage.class);
+        when(responseMessage.isSuccess()).thenReturn(status);
+        when(responseMessage.getErrorCode()).thenReturn(errorCode);
+        return responseMessage;
+    }
+
+    private LogDeliveryMessage getLogDeliveryMessage(boolean status, LogDeliveryErrorCode errorCode) {
+        return new LogDeliveryMessage(1, status, errorCode);
     }
 }
