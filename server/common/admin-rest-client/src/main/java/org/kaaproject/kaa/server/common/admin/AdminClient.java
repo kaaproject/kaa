588cc5ef483cc537c58bd11cf502830ae3bdf382
Andrew Shvayka <ashvayka@cybervisiontech.com>
2015-06-04 11:40:49 +0300

    KAA-1: add method to delete topic to rest client

--- server/common/admin-rest-client/src/main/java/org/kaaproject/kaa/server/common/admin/AdminClient.java
+++ server/common/admin-rest-client/src/main/java/org/kaaproject/kaa/server/common/admin/AdminClient.java
@@ -201,6 +201,12 @@ public class AdminClient {
         ResponseEntity<List<TopicDto>> entity = restTemplate.exchange(url + "topics/"+applicationId, HttpMethod.GET, null, typeRef);
         return entity.getBody();
     }
+    
+    public void deleteTopic(TopicDto ropic) throws Exception {
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("topicId", ropic.getId());
+        restTemplate.postForLocation(url + "delTopic", params);
+    }
 
     public ProfileSchemaDto createProfileSchema(ProfileSchemaDto profileSchema, String schemaResource) throws Exception {
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
