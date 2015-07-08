1011c76ce97c42f5122f89ba4d8b686f29ed5c99
RostakaGmfun <rostawesomegd@gmail.com>
2015-07-08 14:16:30 +0300

    Fixed heap management functions. Added socket implementation

--- client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_mem.h
+++ client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_mem.h
@@ -1,7 +1,8 @@
 #ifndef ESP8266_MEM_H
 #define ESP8266_MEM_H
 
-#include <stdlib.h>
+#include <mem.h>
+
 #ifndef __KAA_MALLOC
 #define __KAA_MALLOC(S) os_malloc(S)
 #endif /* __KAA_MALLOC */
--- client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_sock.h
+++ client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_sock.h
@@ -3,4 +3,18 @@
 
 #include <sys/socket.h>
 
+typedef int kaa_fd_t;
+
+typedef struct sockaddr kaa_sockaddr_t;
+typedef struct sockaddr_in kaa_sockaddr_storage_t;
+typedef socklen_t kaa_socklen_t;
+
+
+#define KAA_HTONS(a)    htons((a))
+#define KAA_HTONL(a)    htonl((a))
+#define KAA_HTONLL(a)   htonll((a))
+
+#define KAA_NTOHS(a)    ntohs((a))
+#define KAA_NTOHL(a)    ntohl((a))
+#define KAA_NTOHLL(a)   ntohll((a))
 #endif /* ESP8266_SOCK_H */
