375a88e8812e10b1e08cd8341154d85afd49ee0e
RostakaGmfun <rostawesomegd@gmail.com>
2015-07-16 11:55:59 +0300

    Added missing files (fix commit 79f66d6661)

--- client/client-multi/client-c/src/kaa/platform/defaults.h
+++ client/client-multi/client-c/src/kaa/platform/defaults.h
@@ -29,7 +29,12 @@
 #ifdef STM32_LEAF_PLATFORM
 #include "../platform-impl/stm32/leafMapleMini/leaf_defaults.h"
 #else
+#ifdef ESP8266_PLATFORM
+#include "../platform-impl/esp8266/esp8266_defaults.h"
+#else
 #include "../platform-impl/posix/posix_defaults.h"
+#endif /* ESP8266_PLATFORM */
+
 #endif //#ifdef STM32_LEAF_PLATFORM
 
 #endif //ifdef ECONAIS_PLATFORM
