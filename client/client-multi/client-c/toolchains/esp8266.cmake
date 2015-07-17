a240c0d60be45458906f8e9d63e841063e883f0e
RostakaGmfun <rostawesomegd@gmail.com>
2015-07-17 14:09:53 +0300

    Added rsa key generation to CMake platform file. Minor fixes

--- client/client-multi/client-c/toolchains/esp8266.cmake
+++ client/client-multi/client-c/toolchains/esp8266.cmake
@@ -5,9 +5,7 @@ set(ESP_TOOLCHAIN_BASE /opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin)
 
 set (ESP_SDK_BASE /opt/Espressif/esp-rtos-sdk)
 
-set(CMAKE_SYSTEM_NAME FreeRTOS)
-
-set(CMAKE_C_COMPILER ${ESP_TOOLCHAIN_BASE}/xtensa-lx106-elf-gcc)
+CMAKE_FORCE_C_COMPILER(${ESP_TOOLCHAIN_BASE}/xtensa-lx106-elf-gcc GNU)
 
 set(CMAKE_LIBRARY_PATH ${ESP_SDK_BASE}/lib/)
 
@@ -18,6 +16,5 @@ set(ESP8266_INCDIRS
     ${ESP_TOOLCHAIN_BASE}/../lib/gcc/xtensa-lx106-elf/4.8.2/include/
     )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-implicit-function-declaration -DESP8266_PLATFORM -Os -Wpointer-arith  -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections")
-
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-implicit-function-declaration -Os -Wpointer-arith  -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections")
 
