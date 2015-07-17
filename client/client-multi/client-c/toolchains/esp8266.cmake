19f16e39bda75eeab6ace59c379b2849d2460db3
RostakaGmfun <rostawesomegd@gmail.com>
2015-07-17 11:50:42 +0300

    Added toolchain file for esp8266.

---
+++ client/client-multi/client-c/toolchains/esp8266.cmake
@@ -0,0 +1,23 @@
+include(CMakeForceCompiler)
+
+#change this to path where you've installed xtensa toolchain
+set(ESP_TOOLCHAIN_BASE /opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin)
+
+set (ESP_SDK_BASE /opt/Espressif/esp-rtos-sdk)
+
+set(CMAKE_SYSTEM_NAME FreeRTOS)
+
+set(CMAKE_C_COMPILER ${ESP_TOOLCHAIN_BASE}/xtensa-lx106-elf-gcc)
+
+set(CMAKE_LIBRARY_PATH ${ESP_SDK_BASE}/lib/)
+
+set(ESP8266_INCDIRS 
+    ${ESP_SDK_BASE}/extra_include ${ESP_SDK_BASE}/include
+    ${ESP_SDK_BASE}/include/lwip ${ESP_SDK_BASE}/include/lwip/ipv4
+    ${ESP_SDK_BASE}/include/lwip/ipv6 ${ESP_SDK_BASE}/include/espressif/
+    ${ESP_TOOLCHAIN_BASE}/../lib/gcc/xtensa-lx106-elf/4.8.2/include/
+    )
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-implicit-function-declaration -DESP8266_PLATFORM -Os -Wpointer-arith  -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections")
+
+
