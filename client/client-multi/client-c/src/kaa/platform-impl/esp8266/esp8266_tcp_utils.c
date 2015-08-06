a22c5786fd4d0bb7df5ea59643b3043f7453cdde
RostakaGmfun <rostawesomegd@gmail.com>
2015-08-06 11:42:13 +0300

    added licence headers

--- client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_tcp_utils.c
+++ client/client-multi/client-c/src/kaa/platform-impl/esp8266/esp8266_tcp_utils.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 CyberVision, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -6,7 +22,6 @@
 #include <lwip/lwip/netdb.h>
 #include <platform/ext_tcp_utils.h>
 #include <kaa_common.h>
-//#include <lwip/lwip/>
 
 kaa_error_t ext_tcp_utils_open_tcp_socket(kaa_fd_t *fd,
                                             const kaa_sockaddr_t *destination,
