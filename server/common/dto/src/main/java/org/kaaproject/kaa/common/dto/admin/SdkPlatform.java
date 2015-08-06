9193f4083aa44928bb1ca64534109cc81277accf
RostakaGmfun <rostawesomegd@gmail.com>
2015-08-06 11:09:51 +0300

    Even more reverting

--- server/common/dto/src/main/java/org/kaaproject/kaa/common/dto/admin/SdkPlatform.java
+++ server/common/dto/src/main/java/org/kaaproject/kaa/common/dto/admin/SdkPlatform.java
@@ -21,8 +21,7 @@ public enum SdkPlatform {
     JAVA("java", "application/java-archive"),
     ANDROID("android", "application/java-archive"),
     CPP("cpp", "application/x-compressed"),
-    C("c", "application/x-compressed"),
-    ESP8266("esp8266", "application/x-compressed");
+    C("c", "application/x-compressed");
 
     String key;
     String contentType;
