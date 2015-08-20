7e85ef682f384013e0f361cc4bdc6842c69c268e
deepnekroz <deepnekro@gmail.com>
2015-08-20 09:54:05 +0300

    Implemented KAA-614 feature: custom LogUploadStrategy Java EP SDK

---
+++ client/client-multi/client-java-core/src/main/java/org/kaaproject/kaa/client/logging/strategies/RecordCountLogUploadStrategy.java
@@ -0,0 +1,43 @@
+package org.kaaproject.kaa.client.logging.strategies;
+
+import org.kaaproject.kaa.client.logging.DefaultLogUploadStrategy;
+import org.kaaproject.kaa.client.logging.LogStorageStatus;
+import org.kaaproject.kaa.client.logging.LogUploadStrategy;
+import org.kaaproject.kaa.client.logging.LogUploadStrategyDecision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Reference implementation for {@link LogUploadStrategy}.
+ * Start log upload when there is recordsThreshold records in storage.
+ */
+public class RecordCountLogUploadStrategy extends DefaultLogUploadStrategy {
+    private static final Logger LOG = LoggerFactory.getLogger(RecordCountLogUploadStrategy.class);
+
+    public RecordCountLogUploadStrategy() {}
+
+    public RecordCountLogUploadStrategy(int countThreshold){
+        this.countThreshold = countThreshold;
+    }
+
+    @Override
+    protected LogUploadStrategyDecision checkUploadNeeded(LogStorageStatus status) {
+        LogUploadStrategyDecision decision = LogUploadStrategyDecision.NOOP;
+
+
+        if(status.getRecordCount() == countThreshold){
+            LOG.info("Need to upload logs - current count: {}, threshold: {}", status.getRecordCount(), countThreshold);
+            decision = LogUploadStrategyDecision.UPLOAD;
+        }
+
+        return decision;
+    }
+
+    public long getRecordsThreshold() {
+        return countThreshold;
+    }
+
+    public void setRecordsThreshold(int countThreshold) {
+        this.countThreshold = countThreshold;
+    }
+}
---
+++ client/client-multi/client-java-core/src/main/java/org/kaaproject/kaa/client/logging/strategies/StorageSizeWithTimeLimitLogUploadStrategy.java
@@ -0,0 +1,66 @@
+package org.kaaproject.kaa.client.logging.strategies;
+
+import org.kaaproject.kaa.client.logging.DefaultLogUploadStrategy;
+import org.kaaproject.kaa.client.logging.LogStorageStatus;
+import org.kaaproject.kaa.client.logging.LogUploadStrategy;
+import org.kaaproject.kaa.client.logging.LogUploadStrategyDecision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Reference implementation for {@link LogUploadStrategy}.
+ *  Start log upload when there storage size is >= minStorageSize bytes or records are stored for more then timeLimit TimeUnit units.
+ */
+public class StorageSizeWithTimeLimitLogUploadStrategy extends DefaultLogUploadStrategy{
+    private static final Logger LOG = LoggerFactory.getLogger(StorageSizeWithTimeLimitLogUploadStrategy.class);
+
+    protected long lastUploadTime;
+    protected TimeUnit timeUnit;
+
+    public StorageSizeWithTimeLimitLogUploadStrategy() {
+        timeUnit = TimeUnit.SECONDS;
+    }
+
+    public StorageSizeWithTimeLimitLogUploadStrategy(int volumeThreshold, long timeLimit, TimeUnit timeUnit) {
+        this.volumeThreshold = volumeThreshold;
+        this.timeLimit = timeLimit;
+        this.timeUnit = timeUnit;
+    }
+
+    @Override
+    protected LogUploadStrategyDecision checkUploadNeeded(LogStorageStatus status) {
+        LogUploadStrategyDecision decision = LogUploadStrategyDecision.NOOP;
+
+        long currentTime = System.currentTimeMillis();
+
+        if(status.getConsumedVolume() >= volumeThreshold){
+            LOG.info("Need to upload logs - current size: {}, threshold: {}", status.getConsumedVolume(), volumeThreshold);
+            decision = LogUploadStrategyDecision.UPLOAD;
+            lastUploadTime = currentTime;
+        }else if(lastUploadTime != 0 && (currentTime - lastUploadTime) >= timeUnit.toMillis(timeLimit)){
+            LOG.info("Need to upload logs - current count: {}, lastUploadedTime: {}, timeLimit: {}", status.getRecordCount(), lastUploadTime, timeLimit);
+            decision = LogUploadStrategyDecision.UPLOAD;
+            lastUploadTime = currentTime;
+        }
+        
+        return decision;
+    }
+
+    public long getVolumeThreshold() {
+        return volumeThreshold;
+    }
+
+    public void setVolumeThreshold(int volumeThreshold) {
+        this.volumeThreshold = volumeThreshold;
+    }
+
+    public long getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(long timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+}
