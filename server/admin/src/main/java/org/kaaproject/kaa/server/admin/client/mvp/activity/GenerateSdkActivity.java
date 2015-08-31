a81fa044d1a205fb9c0143931c618bf8c24b4071
krupet <krutsiuk.petr@gmail.com>
2015-08-31 15:58:26 +0300

    KAA-646: Add message if default user verifier is not specified with Ecf

--- server/admin/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/activity/GenerateSdkActivity.java
+++ server/admin/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/activity/GenerateSdkActivity.java
@@ -175,19 +175,25 @@ public class GenerateSdkActivity extends AbstractDetailsActivity<SdkPropertiesDt
     @Override
     protected void doSave(final EventBus eventBus) {
         onSave();
-        KaaAdmin.getDataSource().generateSdk(entity,new BusyAsyncCallback<String>() {
 
-            @Override
-            public void onFailureImpl(Throwable caught) {
-                Utils.handleException(caught, detailsView);
-            }
+        if (!entity.getAefMapIds().isEmpty() && entity.getDefaultVerifierToken() == null) {
+            detailsView.setErrorMessage("Specify default user verifier.");
+        } else {
 
-            @Override
-            public void onSuccessImpl(String key) {
-                detailsView.clearError();
-                ServletHelper.downloadSdk(key);
-            }
-        });
+            KaaAdmin.getDataSource().generateSdk(entity,new BusyAsyncCallback<String>() {
+
+                @Override
+                public void onFailureImpl(Throwable caught) {
+                    Utils.handleException(caught, detailsView);
+                }
+
+                @Override
+                public void onSuccessImpl(String key) {
+                    detailsView.clearError();
+                    ServletHelper.downloadSdk(key);
+                }
+            });
+        }
     }
     
     @Override
