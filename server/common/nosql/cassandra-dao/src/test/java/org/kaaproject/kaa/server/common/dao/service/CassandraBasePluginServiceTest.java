4d249f61d6ffff6ab847cbc91ec41853723a9582
Viktor Batytskyi <vbatytskyi@cybervisiontech.com>
2015-12-22 18:21:38 +0200

    KAA-624: add cassandra test

---
+++ server/common/nosql/cassandra-dao/src/test/java/org/kaaproject/kaa/server/common/dao/service/CassandraBasePluginServiceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 CyberVision, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kaaproject.kaa.server.common.dao.service;
+
+import org.cassandraunit.CassandraCQLUnit;
+import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/cassandra-dao-test-context.xml")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class CassandraBasePluginServiceTest extends BasePluginServiceTest {
+
+    @ClassRule
+    public static CassandraCQLUnit cassandraUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("cassandra.cql", false, false));
+
+    @AfterClass
+    public static void after() throws Exception {
+        cassandraUnit.cluster.close();
+        while (!cassandraUnit.cluster.isClosed()){}
+    }
+
+    @After
+    public void afterTest() {
+        clearDBData();
+    }
+}
