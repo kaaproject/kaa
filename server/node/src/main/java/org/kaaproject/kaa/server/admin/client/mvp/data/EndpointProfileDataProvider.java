cfc4b2f18d8aa8f8c4874ccb8753bf8e6881521d
Igor Kulikov <ikulikov@cybervisiontech.com>
2016-02-19 15:57:22 +0200

    KAA-863: Fix endpoints search filter

--- server/node/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/data/EndpointProfileDataProvider.java
+++ server/node/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/data/EndpointProfileDataProvider.java
@@ -16,8 +16,11 @@
 
 package org.kaaproject.kaa.server.admin.client.mvp.data;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.view.client.HasData;
+import static org.kaaproject.kaa.server.admin.client.util.Utils.isNotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kaaproject.avro.ui.gwt.client.widget.grid.AbstractGrid;
 import org.kaaproject.kaa.common.dto.EndpointProfileDto;
 import org.kaaproject.kaa.common.dto.EndpointProfilesPageDto;
@@ -27,31 +30,37 @@ import org.kaaproject.kaa.server.admin.client.util.HasErrorMessage;
 import org.kaaproject.kaa.server.admin.shared.services.KaaAdminServiceException;
 import org.kaaproject.kaa.server.admin.shared.services.ServiceErrorCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.view.client.HasData;
+import com.google.gwt.view.client.Range;
 
 public class EndpointProfileDataProvider extends AbstractDataProvider<EndpointProfileDto, String> {
 
     public static final String DEFAULT_OFFSET = "0";
-    private String limit = "20";  // took from AbstractGrid<...>.class see AvroUI
+    private String limit = "20";
     private String pageSize;
     private String offset = DEFAULT_OFFSET;
+    private String applicationId;
     private String groupID;
+    private String endpointKeyHash;
     private List<EndpointProfileDto> endpointProfilesList;
     private int previousStart = -1;
+    private AbstractGrid<EndpointProfileDto, String> dataGrid;
 
-    private static volatile EndpointProfileDataProvider instance;
-
-    private EndpointProfileDataProvider(AbstractGrid<EndpointProfileDto, String> dataGrid, HasErrorMessage hasErrorMessage) {
+    public EndpointProfileDataProvider(AbstractGrid<EndpointProfileDto, String> dataGrid, 
+            HasErrorMessage hasErrorMessage, String applicationId) {
         super(dataGrid, hasErrorMessage, true);
+        this.dataGrid = dataGrid;
+        this.applicationId = applicationId;
         this.groupID = "";
         endpointProfilesList = new ArrayList<>();
         pageSize = limit = (dataGrid.getPageSize() + 1) + "";
     }
-
+    
     @Override
     protected void onRangeChanged(HasData<EndpointProfileDto> display) {
-        if (groupID != null && !groupID.isEmpty()) {
+        if ((endpointKeyHash != null && !endpointKeyHash.isEmpty()) || 
+                (groupID != null && !groupID.isEmpty())) {
             int start = display.getVisibleRange().getStart();
             if (previousStart < start) {
                 previousStart = start;
@@ -63,30 +72,67 @@ public class EndpointProfileDataProvider extends AbstractDataProvider<EndpointPr
 
     @Override
     protected void loadData(final LoadCallback callback) {
-        KaaAdmin.getDataSource().getEndpointProfileByGroupID(groupID, limit, offset,
-                new AsyncCallback<EndpointProfilesPageDto>() {
-                    @Override
-                    public void onFailure(Throwable caught) {
-                        if (caught instanceof KaaAdminServiceException) {
-                            if (((KaaAdminServiceException) caught).getErrorCode() == ServiceErrorCode.ITEM_NOT_FOUND) {
-                                endpointProfilesList.clear();
-                                callback.onSuccess(endpointProfilesList);
-                            }
-                        } else callback.onFailure(caught);
+        if (isNotBlank(endpointKeyHash)) {
+            KaaAdmin.getDataSource().getEndpointProfileByKeyHash(endpointKeyHash, new AsyncCallback<EndpointProfileDto>() {
+                @Override
+                public void onFailure(Throwable caught) {
+                    if (caught instanceof KaaAdminServiceException) {
+                        if (((KaaAdminServiceException) caught).getErrorCode() == ServiceErrorCode.ITEM_NOT_FOUND) {
+                            endpointProfilesList.clear();
+                            callback.onSuccess(endpointProfilesList);
+                        }
+                    } else {
+                        callback.onFailure(caught);
                     }
+                }
 
-                    @Override
-                    public void onSuccess(EndpointProfilesPageDto result) {
-                        endpointProfilesList.addAll(result.getEndpointProfiles());
-                        offset = result.getPageLinkDto().getOffset();
-                        callback.onSuccess(endpointProfilesList);
+                @Override
+                public void onSuccess(EndpointProfileDto endpointProfileDto) {
+                    List<EndpointProfileDto> result = new ArrayList<>();
+                    if (endpointProfileDto.getApplicationId().equals(applicationId)) {
+                        result.add(endpointProfileDto);
                     }
-                });
+                    endpointProfilesList.clear();
+                    endpointProfilesList.addAll(result);
+                    callback.onSuccess(endpointProfilesList);
+                }
+            });
+        } else {
+            KaaAdmin.getDataSource().getEndpointProfileByGroupID(groupID, limit, offset,
+                    new AsyncCallback<EndpointProfilesPageDto>() {
+                        @Override
+                        public void onFailure(Throwable caught) {
+                            if (caught instanceof KaaAdminServiceException) {
+                                if (((KaaAdminServiceException) caught).getErrorCode() == ServiceErrorCode.ITEM_NOT_FOUND) {
+                                    endpointProfilesList.clear();
+                                    callback.onSuccess(endpointProfilesList);
+                                }
+                            } else callback.onFailure(caught);
+                        }
+    
+                        @Override
+                        public void onSuccess(EndpointProfilesPageDto result) {
+                            endpointProfilesList.addAll(result.getEndpointProfiles());
+                            offset = result.getPageLinkDto().getOffset();
+                            callback.onSuccess(endpointProfilesList);
+                        }
+                    });
+        }
     }
 
     public void setNewGroup(String groupID) {
         this.groupID = groupID;
+        this.endpointKeyHash = "";
+    }
+    
+    public void setEndpointKeyHash(String endpointKeyHash) {
+        this.endpointKeyHash = endpointKeyHash;
+    }
+    
+    public void update() {
         reset();
+        dataGrid.getDataGrid().setVisibleRangeAndClearData(
+                new Range(0, dataGrid.getPageSize()), true);        
     }
 
     private void reset() {
@@ -94,13 +140,5 @@ public class EndpointProfileDataProvider extends AbstractDataProvider<EndpointPr
         previousStart = -1;
         limit = pageSize;
         offset = DEFAULT_OFFSET;
-    }
-
-    public static synchronized EndpointProfileDataProvider getInstance(AbstractGrid<EndpointProfileDto, String> dataGrid,
-                                                                       HasErrorMessage hasErrorMessage) {
-        if (instance == null) {
-            instance = new EndpointProfileDataProvider(dataGrid, hasErrorMessage);
-        }
-        return instance;
     }
 }
