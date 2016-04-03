cb7d24603375f8cb1db7de1cb4bece9dbd3e7fe0
Dubland <mshmalko@yandex.ru>
2016-04-03 21:00:07 +0300

    KAA-975 Review disabled tests

--- client/client-multi/client-c/listfiles/UnitTest.cmake
+++ client/client-multi/client-c/listfiles/UnitTest.cmake
@@ -224,3 +224,25 @@ kaa_add_unit_test(NAME test_kaa_reallocation
         test/utilities/test_kaa_reallocation.c
         DEPENDS
         kaac ${OPENSSL_LIBRARIES})
+
+# KAA-985
+#kaa_add_unit_test(NAME test_kaa_tcp_channel_operation
+#        SOURCES
+#        test/kaa_tcp_channel/test_kaa_tcp_channel_operation.c
+#        test/kaa_test_external.c
+#        DEPENDS
+#        kaac ${OPENSSL_LIBRARIES})
+
+# KAA-989
+#kaa_add_unit_test(NAME test_kaa_bootstrap_manager
+#        SOURCES
+#        test/test_kaa_bootstrap_manager.c
+#        DEPENDS
+#        kaac ${OPENSSL_LIBRARIES})
+
+# KAA-988
+#kaa_add_unit_test(NAME test_kaa_channel_manager
+#        SOURCES
+#        test/test_kaa_channel_manager.c
+#        DEPENDS
+#        kaac ${OPENSSL_LIBRARIES})
