e7e31a6e755a0e2cd3e5368e30f284d219f99631
Burdeinyi Oleksandr <aburdeinyi@cybervisiontech.com>
2016-04-11 18:09:36 +0300

    [KAA-993] Added option to build KAA for windows x64 platform; Added MSVC_VERSION variable to suport different Visual Studio versions

--- client/client-multi/client-cpp/tools/env.bat
+++ client/client-multi/client-cpp/tools/env.bat
@@ -16,14 +16,54 @@
 
 @echo off
 
-set MSVS_HOME=C:\Program Files (x86)\Microsoft Visual Studio 12.0
-set BOOST_ROOT=C:\boost
-set AVRO_ROOT_DIR=C:\Avro-cpp
-set BOTAN_HOME=C:\botan
+@REM Set  target Platform(x86|x64)
+set BUILD_PLATFORM=x64
 
-set BOOST_LIBRARYDIR=%BOOST_ROOT%\lib32-msvc-12.0
-set AVRO_LIBRARYDIR=%AVRO_ROOT_DIR%\lib
+@REM Set MSVC++ version from following table
+@REM VS version         - MSVC++ version
+@REM Visual Studio 2015 - 14
+@REM Visual Studio 2013 - 12
+@REM Visual Studio 2012 - 11
+@REM Visual Studio 2010 - 10
+@REM Visual Studio 2008 - 9
+@REM Visual Studio 2005 - 8
 
-set PATH=%MSVS_HOME%\VC;%BOTAN_HOME%;%AVRO_ROOT_DIR%\bin;%AVRO_LIBRARYDIR%;%BOOST_LIBRARYDIR%;%PATH%
+set MSVC_VERSION=14
 
-call vcvarsall.bat
+@REM Set path for thirparty tools
+set MSVS_HOME=C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0
+set BOOST_ROOT=C:\local\boost_1_60_0\
+set PYTHON_HOME=C:\Python27
+set GNUWIN32_HOME=C:\Program Files (x86)\GnuWin32
+
+if not %BUILD_PLATFORM% == x86 (
+  set BUILD_PLATFORM=x64
+)
+
+@REM zlib configuration
+set ZLIB_SRC=zlib-1.2.8
+set ZLIB_HOME=C:\local\%ZLIB_SRC%\%BUILD_PLATFORM%
+set ZLIB_URL=http://zlib.net/zlib-1.2.8.tar.gz
+
+@REM Avro configuration
+set AVRO_SRC=avro-src-1.8.0
+set AVRO_ROOT_DIR=C:\local\%AVRO_SRC%\%BUILD_PLATFORM%
+set AVRO_URL=http://archive.apache.org/dist/avro/avro-1.8.0/avro-src-1.8.0.tar.gz
+
+@REM Botan configuration
+set BOTAN_SRC=Botan-1.11.28
+set BOTAN_HOME=C:\local\%BOTAN_SRC%\%BUILD_PLATFORM%
+set BOTAN_URL=https://github.com/randombit/botan/archive/1.11.28.tar.gz
+
+@REM PATH configuration
+if %BUILD_PLATFORM% == x86 (
+ set BOOST_LIBRARYDIR=%BOOST_ROOT%\lib32-msvc-%MSVC_VERSION%.0
+) else (
+ set BOOST_LIBRARYDIR=%BOOST_ROOT%\lib64-msvc-%MSVC_VERSION%.0
+)
+set BOTAN_INCLUDE_DIR=%BOTAN_HOME%\include
+
+set PATH=%BOTAN_HOME%;%AVRO_ROOT_DIR%\bin;%AVRO_ROOT_DIR%\lib;%ZLIB_HOME%\bin;%ZLIB_HOME%\lib;%PATH%
+set PATH=%MSVS_HOME%\VC;%BOOST_LIBRARYDIR%;%PYTHON_HOME%;%GNUWIN32_HOME%\bin;%PATH%
+
+call vcvarsall.bat %BUILD_PLATFORM%
