c6d93710106663339d67de30a618dc923be0be63
sshapoval <serhii.shapoval@gmail.com>
2016-04-13 11:48:57 +0300

    Fixed copyright header

--- client/client-multi/client-objective-c/Kaa/Kaa.h
+++ client/client-multi/client-objective-c/Kaa/Kaa.h
@@ -1,10 +1,18 @@
-//
-//  Kaa.h
-//  Kaa
-//
-//  Created by Serhii Shapoval on 4/8/16.
-//  Copyright © 2016 CYBERVISION INC. All rights reserved.
-//
+/**
+ *  Copyright 2014-2016 CyberVision, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 
@@ -14,7 +22,6 @@ FOUNDATION_EXPORT double KaaVersionNumber;
 //! Project version string for Kaa.
 FOUNDATION_EXPORT const unsigned char KaaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Kaa/PublicHeader.h>
 
 #import <Kaa/KaaClient.h>
 #import <Kaa/GenericKaaClient.h>
