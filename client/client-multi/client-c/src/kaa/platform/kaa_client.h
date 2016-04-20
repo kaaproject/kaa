d92c167ac0533d198bc43332b342f8ffcd7aec7e
Alexey Shmalko <rasen.dubi@gmail.com>
2016-04-20 16:51:42 +0300

    KAA-1001 Implement -Werror for doxygen

--- client/client-multi/client-c/src/kaa/platform/kaa_client.h
+++ client/client-multi/client-c/src/kaa/platform/kaa_client.h
@@ -55,9 +55,9 @@ kaa_error_t kaa_client_create(kaa_client_t **kaa_client, kaa_client_props_t *pro
 /**
  * @brief De-initializes and destroys Kaa client
  *
- * After a successful call @c kaa_client pointer becomes invalid.
+ * After a successful call @p kaa_client pointer becomes invalid.
  *
- * @param[in]       kaa_context     Pointer to an Kaa client.
+ * @param[in]       kaa_client     Pointer to an Kaa client.
  *
  * @return Error code.
  */
@@ -68,10 +68,10 @@ void kaa_client_destroy(kaa_client_t *kaa_client);
  *
  * Start Kaa client IO loop.
  *
- * @param[in]   kaa_context                  Pointer to an Kaa client.
- * @param[in]    external_process            Callback function to process something outside a main loop
- * @param[in]    external_process_context    Callback function context
- * @param[in]    time_t max_delay            Callback delay
+ * @param[in] kaa_client                  Pointer to an Kaa client.
+ * @param[in] external_process            Callback function to process something outside a main loop
+ * @param[in] external_process_context    Callback function context
+ * @param[in] max_delay                   Callback delay
  *
  * @return Error code.
  */
@@ -85,7 +85,7 @@ kaa_error_t kaa_client_start(kaa_client_t *kaa_client
  *
  * Stop Kaa client IO loop.
  *
- * @param[in]       kaa_context     Pointer to an Kaa client.
+ * @param[in]       kaa_client     Pointer to an Kaa client.
  *
  * @return Error code.
  */
@@ -94,7 +94,7 @@ kaa_error_t kaa_client_stop(kaa_client_t *kaa_client);
 /**
  * @brief Return pointer to Kaa context
  *
- * @param[in]       kaa_context     Pointer to an Kaa client.
+ * @param[in]       kaa_client     Pointer to an Kaa client.
  *
  * @return pointer to Kaa context.
  */
