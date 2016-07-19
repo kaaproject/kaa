8a6ca028bdad119a654c5c221ac7db6cb10a13fd
Kirill Liubun <kreol_123@ukr.net>
2016-07-19 12:20:04 +0300

    Revert: Generate licence

--- server/node/src/test/java/org/kaaproject/kaa/server/node/service/credentials/DefaultCredentialsServiceLocatorTest.java
+++ server/node/src/test/java/org/kaaproject/kaa/server/node/service/credentials/DefaultCredentialsServiceLocatorTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kaaproject.kaa.server.node.service.credentials;
 
 import java.util.List;
