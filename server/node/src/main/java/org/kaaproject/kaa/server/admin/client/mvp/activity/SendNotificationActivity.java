dfd7ea28784e1f144d1f8efa752879e9f6d2bc82
pyshankov <pyshankov@gmail.com>
2016-09-14 17:57:20 +0300

    KAA-1191: EndpointKeyHash field is not refreshed in Notificaion topic

--- server/node/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/activity/SendNotificationActivity.java
+++ server/node/src/main/java/org/kaaproject/kaa/server/admin/client/mvp/activity/SendNotificationActivity.java
@@ -71,7 +71,7 @@ public class SendNotificationActivity extends AbstractDetailsActivity<Notificati
 
     @Override
     protected void onEntityRetrieved() {
-        detailsView.getEndpointKeyHash().setValue("");rm -r
+        detailsView.getEndpointKeyHash().setValue("");
         KaaAdmin.getDataSource().getUserNotificationSchemaInfosByApplicationId(applicationId, 
                 new BusyAsyncCallback<List<SchemaInfoDto>>() {
             @Override
