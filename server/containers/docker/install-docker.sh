a4b381a5b797402eb10af25faedd669c2367e6a1
vchizhevsky <vchizhevsky@cybervisiontech.com>
2016-09-27 12:30:29 +0300

    KAA-1237: Docker. Added license.

--- server/containers/docker/install-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Install docker and docker compose
-    wget -qO- https://get.docker.com/ | sh
-    su -i
-    curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    exit
--- server/containers/docker/using-compose/launch-kaa.sh
+++ server/containers/docker/using-compose/launch-kaa.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-
+#
+# Copyright 2014-2016 CyberVision, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 DATABASE_SQL_NOSQL=$1
 KAA_NODE_SCALE=$2
 INTERNAL_LOCALHOST=$3
@@ -70,8 +84,8 @@ configureAndStartThirdPartyComponents() {
                     exit 1
                     ;;
             esac
-            sed -i -e "6s@.*@    image: ${SQL_PROVIDER_NAME}@" third-party-docker-compose.yml
-            sed -i -e "19s@.*@    image: ${NOSQL_PROVIDER_NAME}@" third-party-docker-compose.yml
+            sed -i -e "21s@.*@    image: ${SQL_PROVIDER_NAME}@" third-party-docker-compose.yml
+            sed -i -e "34s@.*@    image: ${NOSQL_PROVIDER_NAME}@" third-party-docker-compose.yml
 
             docker-compose -f third-party-docker-compose.yml up -d
         fi
@@ -125,7 +139,7 @@ configureClusterNodesKaa() {
         KAA_ADMIN_UI_PORT+=( $(( DEFAULT_ADMIN_PORT + $i * 100 )) )
         STRING_NAMES_NODES_KAA+=(${DEFAULT_KAA_SERVICE_NAME}${i})
         KAA_ADMIN_UI_PORTS+=(${KAA_ADMIN_UI_PORT})
-        ex -sc "25i|`cat kaa-docker-compose.yml.template | tail -37 | head -22 | sed \
+        ex -sc "41i|`cat kaa-docker-compose.yml.template | tail -37 | head -22 | sed \
         -e "s|{{KAA_SERVICE_NAME}}|${DEFAULT_KAA_SERVICE_NAME}${i} |g" \
         -e "s|{{ADMIN_PORT}}|${KAA_ADMIN_UI_PORT}|g" \
         -e "s|{{BOOTSTRAP_TCP}}|$(( DEFAULT_BOOTSTRAP_TCP + $i * 100 ))|g" \
@@ -175,7 +189,7 @@ else
         else
             INTERNAL_HOST_KAA=`docker inspect --format='{{(index (index .NetworkSettings.Networks ) "usingcompose_front-tier").IPAddress}}' usingcompose_${DEFAULT_KAA_SERVICE_NAME}${j}_1`
             (( j++ ))
-            ex -sc "31i|`cat kaa-nginx-config/nginx.conf.template | tail -5 | head -1 | sed \
+            ex -sc "47i|`cat kaa-nginx-config/nginx.conf.template | tail -5 | head -1 | sed \
             -e "s|{{PROXY_HOST_KAA}}|${INTERNAL_HOST_KAA}|g" \
             -e "s|{{PROXY_PORT}}|${i}|g" \
             `" -cx kaa-nginx-config/kaa-nginx.conf
