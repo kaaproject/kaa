a81cafd2f59dfedaa3a8f3e9be46d6d683a135dc
vchizhevsky <vchizhevsky@cybervisiontech.com>
2016-09-28 15:39:17 +0300

    KAA-1237: Docker. Docker file fixed.

--- server/containers/docker/using-compose/view-errors.sh
+++ server/containers/docker/using-compose/view-errors.sh
@@ -16,8 +16,8 @@
 #
 KAA_SERVICE_NAME=$1
 
-if [ -z "$KAA_SERVICE_NAME" ]; then
-    docker-compose -p usingcompose exec $KAA_SERVICE_NAME cat /var/log/kaa/* | grep ERROR
+if ! [ -z "$KAA_SERVICE_NAME" ]; then
+    docker-compose -f kaa-docker-compose.yml -p usingcompose exec $KAA_SERVICE_NAME sh /kaa/cat-error.sh
 else
     echo "Please specify KAA_SERVICE_NAME which is specified in the 'kaa-docker-compose.yml' file."
 fi
--- server/containers/docker/using-compose/view-kaa-node-logs.sh
+++ server/containers/docker/using-compose/view-kaa-node-logs.sh
@@ -16,8 +16,8 @@
 #
 KAA_SERVICE_NAME=$1
 
-if [ -z "$KAA_SERVICE_NAME" ]; then
-    docker-compose -p usingcompose exec $KAA_SERVICE_NAME sh /kaa/tail-node.sh
+if ! [ -z "$KAA_SERVICE_NAME" ]; then
+    docker-compose -f kaa-docker-compose.yml -p usingcompose exec $KAA_SERVICE_NAME sh /kaa/tail-node.sh
 else
     echo "Please specify KAA_SERVICE_NAME which is specified in the 'kaa-docker-compose.yml' file."
 fi
